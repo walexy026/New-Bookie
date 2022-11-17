a30143a7bf0d1e53799f949cdf222f3646683885
olawale <jao.olaniyi@gmail.com>
2022-11-17 13:48:43 +0100

    added icons from iconify

--- src/Components/Sidebars/Sidebars.jsx
+++ src/Components/Sidebars/Sidebars.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Sidebars.css";
-
+import { Icon } from '@iconify/react';
 
 const Sidebars = () => {
   return (
@@ -10,12 +10,12 @@ const Sidebars = () => {
         <div className="sidebarWrapper">
         <div className="sidebarMenu">
         <ul className="sidebarList">
-          <li className="sidebarListItem active"> Overview</li>
-          <li className="sidebarListItem "> Savings</li>
-          <li className="sidebarListItem "> Wallets</li>
-          <li className="sidebarListItem ">   Refferal</li>
-          <li className="sidebarListItem ">Profile</li>
-          <li className="sidebarListItem ">  Settings</li>
+          <li className="sidebarListItem active"><Icon icon="icon-park:application-two" /> Overview</li>
+          <li className="sidebarListItem "> <Icon icon="material-symbols:savings-rounded" /> Savings</li>
+          <li className="sidebarListItem "> <Icon icon="ion:wallet" /> Wallets</li>
+          <li className="sidebarListItem "> <Icon icon="icon-park:add-user" />  Refferal</li>
+          <li className="sidebarListItem "><Icon icon="pajamas:profile" /> Profile</li>
+          <li className="sidebarListItem "><Icon icon="material-symbols:settings-rounded" />  Settings</li>
           </ul>
         </div>
         </div>
