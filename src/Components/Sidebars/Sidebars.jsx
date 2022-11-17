5f8fa72427995bcaa5d442c8ba33853f70a73f57
olawale <jao.olaniyi@gmail.com>
2022-11-17 14:20:35 +0100

    imported bree serif font from google fonts

--- src/Components/Sidebars/Sidebars.jsx
+++ src/Components/Sidebars/Sidebars.jsx
@@ -10,12 +10,12 @@ const Sidebars = () => {
         <div className="sidebarWrapper">
         <div className="sidebarMenu">
         <ul className="sidebarList">
-          <li className="sidebarListItem active"><Icon icon="icon-park:application-two" /> Overview</li>
-          <li className="sidebarListItem "> <Icon icon="material-symbols:savings-rounded" /> Savings</li>
-          <li className="sidebarListItem "> <Icon icon="ion:wallet" /> Wallets</li>
-          <li className="sidebarListItem "> <Icon icon="icon-park:add-user" />  Refferal</li>
-          <li className="sidebarListItem "><Icon icon="pajamas:profile" /> Profile</li>
-          <li className="sidebarListItem "><Icon icon="material-symbols:settings-rounded" />  Settings</li>
+          <li className="sidebarListItem active"><Icon icon="icon-park:application-two" fontSize='1.5rem' style={{marginRight:'1.5rem'}}  /> Overview</li>
+          <li className="sidebarListItem "> <Icon icon="material-symbols:savings-rounded" fontSize='1.5rem' style={{marginRight:'1.5rem'}} /> Savings</li>
+          <li className="sidebarListItem "> <Icon icon="ion:wallet" fontSize='1.5rem'  style={{marginRight:'1.5rem'}}/> Wallets</li>
+          <li className="sidebarListItem "> <Icon icon="icon-park:add-user" fontSize='1.5rem' style={{marginRight:'1.5rem'}} />  Refferal</li>
+          <li className="sidebarListItem "><Icon icon="pajamas:profile" fontSize='1.5rem' style={{marginRight:'1.5rem'}}/> Profile</li>
+          <li className="sidebarListItem "><Icon icon="material-symbols:settings-rounded" fontSize='1.5rem' style={{marginRight:'1.5rem'}} />  Settings</li>
           </ul>
         </div>
         </div>
