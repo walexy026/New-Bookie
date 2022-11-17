c5f89058c0163b9ef750ac3e8c9c4c872c8059c9
olawale <jao.olaniyi@gmail.com>
2022-11-17 17:53:42 +0100

    added widget continued

--- src/Components/RecentTransactions/RecentTransactions.jsx
+++ src/Components/RecentTransactions/RecentTransactions.jsx
@@ -18,12 +18,8 @@ const RecentTransactions = () => {
         </tr>
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
-            <img
-              src="https://images.gr-assets.com/authors/1561336084p8/4123863.jpg"
-              alt=""
-              className="widgetLgImg"
-            />
             <span className="widgetLgName">Mobile Transfer </span>
+            <span className='widgetSmName'>Added to my funds</span>
           </td>
           <td className="widgetLgDate">NGN 450,00 </td>
           <td className="widgetLgAmount">Nov8,2023</td>
@@ -33,12 +29,8 @@ const RecentTransactions = () => {
         </tr>
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
-            <img
-              src="https://media-exp1.licdn.com/dms/image/C4D03AQFsohsj7miS2w/profile-displayphoto-shrink_800_800/0/1651377283409?e=1657756800&v=beta&t=rHN6C2GAljYKMZKQkAhR5DZyXfGtPv2r3PCo0Zqw7aU"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Ikechi Fortune</span>
+          <span className="widgetLgName">Mobile Transfer </span>
+            <span className='widgetSmName'>Added to my funds</span>
           </td>
           <td className="widgetLgDate">12 May 2022</td>
           <td className="widgetLgAmount">$1202.00</td>
@@ -48,12 +40,8 @@ const RecentTransactions = () => {
         </tr>
         <tr className="widgetLgTr">
           <td className="widgetLgUser">
-            <img
-              src="https://media-exp1.licdn.com/dms/image/C4D03AQFsohsj7miS2w/profile-displayphoto-shrink_800_800/0/1651377283409?e=1657756800&v=beta&t=rHN6C2GAljYKMZKQkAhR5DZyXfGtPv2r3PCo0Zqw7aU"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Ikechi Fortune</span>
+          <span className="widgetLgName">Mobile Transfer </span>
+            <span className='widgetSmName'>Added to my funds</span>
           </td>
           <td className="widgetLgDate">12 May 2022</td>
           <td className="widgetLgAmount">$1202.00</td>
