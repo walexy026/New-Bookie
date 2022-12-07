34f726aeb13b0c6c12f56dd3e4273cca34f27643
Martins <daramola.martins03@gmail.com>
2022-12-07 07:49:28 +0100

    Books api fetched

--- src/books/Books.jsx
+++ src/books/Books.jsx
@@ -1,35 +1,50 @@
-// import React , {useEffect, useState} from 'react'
+import React , {useEffect, useState} from 'react'
 import { BsStarFill } from "react-icons/bs"
-import data from './bookData'
+// import data from './bookData'
 import './Book.css'
 
 
-
+const url = 'https://bookie-app.onrender.com/api/books'
 
 export default function Books() {
+    const [data, setData] = useState([]);
+    const getData = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+    };
+    useEffect(()=> {
+        getData();
+    }, [])
+
  
     
     return (
         <section className="book-section">
             <div className="book-card-container">
-                {data.map(({id,imgUrl,price,title,description}) => {
+                {data.map(({id,bookName,price,bookImage,bookPublisher,categories}) => {
                     return (
                         <div className="bookImage-container" >
-                            <img src= {imgUrl}alt="" className="book-items" key={id} />
+                            <img src= {bookImage}alt="" className="book-items" key={id} />
                             <div className="book-section-title-container">
                                 <h2 className="book-section-title">
-                                    {title}
+                                    {bookName}
                                     
                                 </h2>
+    
                                 <div className="description-price-container">
-                                    <p className="book-description">{description}</p>
+                                    <p className="book-description">{categories}</p>
                                     <p className='book-price-description'>{price}</p>
                                 </div>
+                                <div className='description-price-container'>
+                                <h4 className='book-description'>{bookPublisher}</h4>
+                                <div>
                                 <BsStarFill className='star-icon'/>
                                 <BsStarFill className='star-icon'/>
                                 <BsStarFill className='star-icon'/>
                                 <BsStarFill className='star-icon'/>
-                                <BsStarFill className='star-icon'/>
+                                <BsStarFill className='star-icon'/></div>
+                                </div>
                             </div>
                         </div>
                     )
