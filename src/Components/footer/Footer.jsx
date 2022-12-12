bb4add48856fb6d5733b6a6233d8980179b4832a
olawale <jao.olaniyi@gmail.com>
2022-12-12 13:15:39 +0100

    workimg on the mobile view of the footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,50 +1,149 @@
-import footerLogo from '../../Assets/logo.svg'
-import { Link } from 'react-router-dom'
-import FooterCSS from './Footer.module.css'
+import footerLogo from "../../Assets/logo.svg";
+import { Link } from "react-router-dom";
+import FooterCSS from "./Footer.module.css";
+import { useState } from "react";
+
 export default function Footer() {
+  const [visLinks, setVisLinks] = useState(false);
+  const [visAccount, setVisAccount] = useState(false);
+  const [visSocials, setVisSocials] = useState(false);
+  const toggleLinks = () => {
+    setVisLinks(!visLinks);
+  };
+  const toggleAccount = () => {
+    setVisAccount(!visAccount);
+  };
+  const toggleSocials = () => {
+    setVisSocials(!visSocials);
+  };
+
   return (
-<section className= {FooterCSS.bookieFooter}>
-    <div className={FooterCSS.footerContainer}>
+    <footer className={FooterCSS.bookieFooter}>
+      <div className={FooterCSS.footerContainer}>
         <div className={FooterCSS.logobox}>
-            <img src= {footerLogo} alt="footer-logo" className={FooterCSS.footerLogo} />
-            <p className= {FooterCSS.Brandname}>Bookie</p>
+          <img
+            src={footerLogo}
+            alt="footer-logo"
+            className={FooterCSS.footerLogo}
+          />
+          <p className={FooterCSS.Brandname}>Bookie</p>
         </div>
-        
-         <div className={FooterCSS.footerContent}>
-            <p className={FooterCSS.content}>Save Money in other to  Buy Books for your children </p>
-            <p className={FooterCSS.address}>10, Publish Estate, Alakori Street, Abeokuta Ogun State</p>
-            <div className={FooterCSS.address}>bookie@gmail.com</div>
-     </div>
-    </div>
-        <div className={FooterCSS.listContainer}>
-            <div className={FooterCSS.list}>
-                <p> Links</p>
-                <ul>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>Contact Us</Link></li>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>About Us</Link></li>
-                    <li><Link to = '#' className= {FooterCSS.footerLink} >Direction</Link></li>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>Blog</Link></li>
-                </ul>
-            </div>
-            <div className={FooterCSS.list}>
-                 <p> My Account</p>
-                <ul>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>Profile</Link></li>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>Settings</Link></li>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>Track History</Link></li>
-                    <li><Link to = '#'className= {FooterCSS.footerLink}>Renew Subcription</Link></li>
-                </ul>
-            </div>
-            <div className={FooterCSS.list}>
-                 <p>Social</p>
-                <ul>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>Instagram</Link></li>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>Facebook</Link></li>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>Twitter</Link></li>
-                    <li><Link to = '#' className= {FooterCSS.footerLink}>Linkedin</Link></li>
-                </ul>
-            </div>
+
+        <div className={FooterCSS.footerContent}>
+          <p className={FooterCSS.content}>
+            Save Money in other to Buy Books for your children{" "}
+          </p>
+          <address>
+            10, Publish Estate, Alakori Street, Abeokuta Ogun State
+          </address>
+          <div className={FooterCSS.address}>bookie@gmail.com</div>
         </div>
-</section>
-  )
+      </div>
+      <div className={FooterCSS.listContainer}>
+        <div className={FooterCSS.list}>
+          <div className="firstlist" onClick={toggleLinks}>
+            Link
+          </div>
+
+          {visLinks && (
+            <ul>
+              {/* <li  >
+              Links
+            </li> */}
+
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Contact Us
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Direction
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+        <div className={FooterCSS.list}>
+          <div className="firstlist" onClick={toggleAccount}>
+            {" "}
+            My Account
+          </div>
+          {visAccount && (
+            <ul>
+              <li>
+                {/* <Link to="#" className={FooterCSS.footerLink}> */}
+
+                {/* </Link> */}
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Settings
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Track History
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Renew Subcription
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+        <div className={FooterCSS.list}>
+          <div className="firstlist" onClick={toggleSocials}>
+            Social
+          </div>
+          {visSocials && (
+            <ul>
+              <li>
+                {/* <Link to="#" className={FooterCSS.footerLink}> */}
+
+                {/* </Link> */}
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Instagram
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Facebook
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Twitter
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className={FooterCSS.footerLink}>
+                  Linkedin
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </footer>
+  );
 }
